baeb7b9d763807060fa128f45f242ff1e8409a25
Timothy Keating <timothykeating@gmail.com>
2021-11-19 11:38:10 -0500

    Uploading coursework

--- scraping.py
+++ scraping.py
@@ -6,13 +6,31 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
 
 ###
 # Set-Ups
 
-# Set up Splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+    # headless True means the browser will not phyiscally open so we can watch it
+
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary
+    data = {
+      "news_title": news_title,
+      "news_paragraph": news_paragraph,
+      "featured_image": featured_image(browser),
+      "facts": mars_facts(),
+      "last_modified": dt.datetime.now()
+      }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data  
 
 
 ###
@@ -44,12 +62,12 @@ def mars_news(browser):
         news_title = slide_elem.find('div', class_='content_title').get_text()
 
         # now use similar code to scrape the teaser paragraph
-        news_p = slide_elem.find('div',class_='article_teaser_body').get_text()
+        news_paragraph = slide_elem.find('div',class_='article_teaser_body').get_text()
 
     except AttributeError:
         return None, None
 
-    return news_title, news_p
+    return news_title, news_paragraph
 
 def featured_image(browser):
 
@@ -75,7 +93,7 @@ def featured_image(browser):
     
            
     # append this to the base url for a full url
-    img_url_full = f'https://https://spaceimages-mars.com/{img_url}'
+    img_url_full = f'https://spaceimages-mars.com/{img_url}'
     
     return img_url_full
 
@@ -93,17 +111,7 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
 
+if __name__ == "__main__":
 
-# Now you're ready to integrate Mongo.
-# 10.5.3 - # Integrate MongoDB Into the Web App
-###
-
-
-
-
-
-
-
-
-
-browser.quit()
+    # If running as script, print scraped data
+    print(scrape_all())
